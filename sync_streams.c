26af2bd95e156ff39e3af48f05f3d6961f4f2cc0
ushchenko <danyodesa@gmail.com>
2022-06-03 01:48:18 +0300

    Create by local git

---
+++ sync_streams.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <pthread.h>
+
+int turn = 0;
+int main() {
+        pthread_t f2_thread, f1_thread;
+        void *f2(), *f1();
+        pthread_create(&f1_thread, NULL, f1, NULL);
+        pthread_create(&f2_thread, NULL, f2, NULL);
+        pthread_join(f1_thread, NULL);
+        pthread_join(f2_thread, NULL);
+
+        return 0;
+}
+
+void *f1() {
+        while(1) {
+                while (turn != 0);
+                sleep(2);
+                printf("f1: critical Ushchenko\n");
+                turn = 1;
+                printf("f1: noncritical Ushchenko\n");
+        }
+}
+
+void *f2() {
+        while(1) {
+                sleep(1);
+                while (turn == 0);
+                printf("f2: critical Ushchenko\n");
+                sleep(3);
+                turn = 0;
+                printf("f2: noncritical Ushchenko\n");
+        }
+}
